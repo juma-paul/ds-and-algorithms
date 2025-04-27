48dd30dda03660756950454d39f19d2daa23190c
juma <raisben7@gmail.com>
2025-04-27 01:46:31 -0700

    Add brute force solution for two sum

---
+++ src/two_sum.py
@@ -0,0 +1,14 @@
+from typing import List
+
+# Brute Force Solution [Time: O(n^2) | Space: O(1)]
+
+def two_sum_brute(nums: List[int], target: int) -> List[int]:
+    n = len(nums)
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+
+
+    
